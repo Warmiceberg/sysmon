331cc989f6063f737624dfb9cf0ec381073f6241
iamsudip <iamsudip@programmer.net>
2013-09-23 23:24:20 +0530

    Updated code to use sql database

--- sysinfo.py
+++ sysinfo.py
@@ -8,40 +8,59 @@
 import os
 import time
 import psutil
-import json
 
 def uptime():
-    date_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    date_time = time.strftime("%Y%m%d %H%M%S", time.localtime())
     date = date_time.split()[0]
     ptime = date_time.split()[1]
     with open('/proc/uptime', 'r') as f:
-        data = float(f.read().split()[0])
-        hour, minute = int(data/3600), int(data%3600/60)
+        uptime_data = float(f.read().split()[0])
     min1, min5, min15 = os.getloadavg()
-    print "date: %s" "current time: %s uptime: %s:%s load average: %s, %s, %s" %(date, ptime, hour, minute, min1, min5, min15)
+    up_time = {
+               "Date" : date,
+               "Current time" : ptime,
+               "Uptime" : uptime_data,
+               "min1avg" : min1,
+               "min5avg" : min5,
+               "min15avg" : min15
+              }
+    return up_time
 
 def mem_data():
-    kb = 1024
     vert_mem = psutil.virtual_memory()
-    print "Mem: " + str(vert_mem.total/kb) + "k total, " + str(vert_mem.used/kb) + "k used, " + str(vert_mem.free/kb) + "k free, " + str(vert_mem.buffers/kb) + "k buffers"
     swap_mem = psutil.swap_memory()
-    print "Swap: " + str(swap_mem.total/kb) + "k total, " + str(swap_mem.used/kb) + "k used, " + str(swap_mem.free/kb) + "k free, "
+    memory_data = {
+                   "Virtual memory" : {
+                                "Total" : vert_mem.total,
+                                "Used" : vert_mem.used,
+                                "Free" : vert_mem.free,
+                                "Buffers" : vert_mem.buffers
+                               },
+                   "Swap memory" : {
+                                "Total" : swap_mem.total,
+                                "Used" : swap_mem.used,
+                                "Free" : swap_mem.free
+                              }
+                  }
+    return memory_data
 
 def process_data():
-    print "  PID        OWNER    NI        VIRT(kb)     RES(kb)    STATUS   %MEM     TIME+(sec)                  NAME"
+    p_data = []
     for process in psutil.process_iter():
-        process_pid = process.pid
-        process_owner = process.username
-        process_virt = int(process.get_memory_info().vms)/1024
-        process_res = int(process.get_memory_info().rss)/1024
-        process_mempercent = process.get_memory_percent()
-        process_cputime = process.get_cpu_times().user + process.get_cpu_times().system
-        process_name = process.name
-        process_status = process.status
-        process_nice_value = process.get_nice()
-        print "%5s %12s %5s %12s %12s %12s %5.2f %10s %25s" %(process_pid, process_owner, process_nice_value, process_virt, process_res, process_status, process_mempercent, process_cputime, process_name) 
+        p_data.append({
+                            "PID" : process.pid,
+                            "USER": process.username,
+                            "NI" : process.get_nice(),
+                            "VIRT" : int(process.get_memory_info().vms)/1024,
+                            "RES" : int(process.get_memory_info().rss)/1024,
+                            "STATE" : process.status,
+                            "TIME" : process.get_cpu_times().user + process.get_cpu_times().system,
+                            "NAME" : process.name,
+                            "MEMPERCENT" : process.get_memory_percent(),
+                           })  
+    return p_data
 
 if __name__ == '__main__':
-         uptime()
-         mem_data()
-         process_data()
+    ut = uptime()
+    md = mem_data()
+    pd = process_data()
